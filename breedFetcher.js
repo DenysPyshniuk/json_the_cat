52c2dd59be6771c2b6d07ed8597609b064d0be9e
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-12-03 22:50:11 +0000

    Refactored and tested

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,22 @@
 const request = require("request");
 
-const whatBreed = process.argv[2];  //=> instead of slice going to get specific index
-const adressRequest = `https://api.thecatapi.com/v1/breeds/search?q=${whatBreed}`;
 
-request(adressRequest, (error, response, body) => {
-  const data = JSON.parse(body);
-  if(error) {
-    console.log("unfortunately, error heppened: ", error); // Print the error if one occurred
-  }
-  if (whatBreed === undefined) {
-    console.log("Failed! Can't find such breed..."); // Print the error if one occurred
-  } else {
-    console.log("Sucsesfully requested!");
-    console.log(adressRequest, data[0]); //=> console log content from the adressRequest if everything ok
+const fetchBreedDescription = function(breedName, callback) {
+
+  const adressRequest = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(adressRequest, (error, response, body) => {
+    if (error) {
+      callback(`Unfortunately, error heppened: ${error}`, null); // Print the error if one occurred
     }
-});
+    const data = JSON.parse(body);
+    const breed = data[0];
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback("Can't find such breed", null);
+    }
+  });
+};
+
+module.exports = {fetchBreedDescription};
